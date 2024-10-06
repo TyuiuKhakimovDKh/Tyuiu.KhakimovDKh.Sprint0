ddf04c0f6c8fe164085f99136f68bdf58c61086b
Достонбек Хакимов <Tyuiu.KhakimovDkh@outlook.com>
2024-10-06 16:58:31 +0500

    Task1##

---
+++ Tyuiu.KhakimovDKh.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.KhakimovDKh.Sprint0.Task1.V0;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+     
+        Console.WriteLine("Hello, World!");
+    }    {
+
+    public Program()
+        
+    }
+
+}
